8b8a43ff79d0f86a88297d137e7aa1d0c51a93e8
Maks Shkvirya <Maks Shkvirya>
2014-04-29 16:32:11 +0300

    changes

--- src/com/max/TestCalculator.java
+++ src/com/max/TestCalculator.java
@@ -4,6 +4,7 @@ import com.football.*;
 public class TestCalculator {
     public static void main(String[] args) {
         Calculator example=new Calculator(5.5);
-        example.sum(5);
+        System.out.println(example.sum(0.5));
+        System.out.println(example.division(0));
     }
 }
